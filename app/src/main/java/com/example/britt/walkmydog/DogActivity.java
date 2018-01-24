3fcefb35286500d1baa10f698e87fddaf5e02773
Britt0508 <britt2609@hotmail.com>
2018-01-24 16:57:57 +0100

    Sync alles. App bijna klaar, alleen nog kleine foutjes eruit halen. Locatie weergeven, met zoom. Eigen locatie nog fixen (idee ervan staat al gecommmend)

--- app/src/main/java/com/example/britt/walkmydog/DogActivity.java
+++ app/src/main/java/com/example/britt/walkmydog/DogActivity.java
@@ -1,10 +1,16 @@
 package com.example.britt.walkmydog;
 
+import android.*;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
@@ -18,13 +24,17 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -51,6 +61,9 @@ public class DogActivity extends AppCompatActivity implements OnMapReadyCallback
     String dog;
     String id;
 
+    Double latitude;
+    Double longitude;
+
     TextView nameText;
     TextView descriptionText;
     ImageView picture;
@@ -62,19 +75,15 @@ public class DogActivity extends AppCompatActivity implements OnMapReadyCallback
 
     private DatabaseReference databaseReference;
 
+//    int myLocationRequestCode = 100;
+
     private FirebaseAuth mAuth;
 
     private static final float DEFAULT_ZOOM = 15f;
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        location = new LatLng(lat, lon);
-        googleMap.addMarker(new MarkerOptions().position(location)
-                .title("Marker of position of the dog's boss"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location));
-        moveCamera(new LatLng(lat, lon),
-                DEFAULT_ZOOM);
-    }
+//    vars
+//    private Boolean mLocationPermissionsGranted = false;
+//    private GoogleMap mMap;
+//    private FusedLocationProviderClient mFusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -101,25 +110,50 @@ public class DogActivity extends AppCompatActivity implements OnMapReadyCallback
         bossID = intent.getStringExtra("bossID");
 
         getFromDB();
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+//        if (ActivityCompat.checkSelfPermission(DogActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(DogActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, myLocationRequestCode);
+//        }
+//        showSettingAlert();
+//        Log.d("TAGG", "LOCATIEEEE");
+//        mLocationPermissionsGranted = true;
+//        getLocation();
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        location = new LatLng(lat, lon);
+
+        mMap.addMarker(new MarkerOptions().position(location)
+                .title("Marker of position of the dog's boss"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+
+        Log.w("logtag", "" + location);
+
+        moveCamera(new LatLng(lat, lon),
+                DEFAULT_ZOOM);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+    }
+
 
     public void SelectOption(View view) {
         String option = spinner.getSelectedItem().toString();
-        if(option.equals("Log Out"))
-        {
+        if (option.equals("Log Out")) {
             FirebaseAuth.getInstance().signOut();
             Intent intent = new Intent(DogActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
-        }
-        else if (option.equals("Overview"))
-        {
+        } else if (option.equals("Overview")) {
             Intent intent = new Intent(DogActivity.this, OverviewActivity.class);
             startActivity(intent);
             finish();
-        }
-        else if (option.equals("Adverts"))
-        {
+        } else if (option.equals("Adverts")) {
             Intent intent = new Intent(DogActivity.this, ChooseActivity.class);
             startActivity(intent);
             finish();
@@ -161,16 +195,14 @@ public class DogActivity extends AppCompatActivity implements OnMapReadyCallback
                     if (!favo.contains(mDog)) {
                         favo.add(mDog);
                     }
-                }
-
-                else {
+                } else {
                     Log.d("else array", mDog.getName());
                     favo = new ArrayList<>();
                     favo.add(mDog);
                 }
                 Log.d("arraylist", favo.toString());
                 databaseReference.child("users").child(id).child("favorites").setValue(favo);
-        }
+            }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
@@ -180,12 +212,10 @@ public class DogActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
 
-
     static void getImage(String photo, ImageView picture) {
         if (photo == null) {
             Log.w("LOGO", "Logo is used");
-        }
-        else {
+        } else {
             byte[] decodedString = Base64.decode(photo, Base64.DEFAULT);
             Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
             picture.setImageBitmap(decodedByte);
@@ -193,16 +223,91 @@ public class DogActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
     public void makeAppointment(View view) {
-        Intent intent = new Intent (DogActivity.this, ContactActivity.class);
+        Intent intent = new Intent(DogActivity.this, ContactActivity.class);
         setFavorites();
         intent.putExtra("bossID", bossID);
         intent.putExtra("dog", dog);
         startActivity(intent);
     }
 
-    private void moveCamera(LatLng latLng, float zoom){
-        Log.d("TAGG", "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude );
+    private void moveCamera(LatLng latLng, float zoom) {
+        Log.d("TAGG", "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
     }
-
 }
+
+//    public void getLocation() {
+//
+//        Log.d("TAGG", "getDeviceLocation: getting the devices current location");
+//
+//        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+//
+//        try{
+//            if(mLocationPermissionsGranted){
+//
+//                final Task location = mFusedLocationProviderClient.getLastLocation();
+//                location.addOnCompleteListener(new OnCompleteListener() {
+//                    @Override
+//                    public void onComplete(@NonNull Task task) {
+//                        if(task.isSuccessful()){
+//                            Location currentLocation = (Location) task.getResult();
+//
+//                            if (!(task.getResult() == null)) {
+//                                latitude = currentLocation.getLatitude();
+//                                longitude = currentLocation.getLongitude();
+//                            }
+//
+//                        }else{
+//                            Log.d("TAGG", "onComplete: current location is null");
+//                            Toast.makeText(DogActivity.this, "unable to get current location", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                });
+//            }
+//        }catch (SecurityException e){
+//            Log.e("TAGG", "getDeviceLocation: SecurityException: " + e.getMessage() );
+//        }
+//    }
+//
+//    public void showSettingAlert()
+//    {
+//        String locationProviders = Settings.Secure.getString(getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+//        if (locationProviders == null || locationProviders.equals("")) {
+//            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+//            alertDialog.setTitle("GPS setting!");
+//            alertDialog.setMessage("GPS is not enabled, Do you want to go to settings menu? ");
+//            alertDialog.setPositiveButton("Setting", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+//                    DogActivity.this.startActivity(intent);
+//                }
+//            });
+//            alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    dialog.cancel();
+//                }
+//            });
+//            alertDialog.show();
+//        }
+//    }
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        if (requestCode == myLocationRequestCode) {
+//
+//            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//
+//                Toast.makeText(this, "location permission granted", Toast.LENGTH_LONG).show();
+//                getLocation();
+//            } else {
+//
+//                Toast.makeText(this, "location permission denied", Toast.LENGTH_LONG).show();
+//
+//            }
+//        }
+//    }
+//}
